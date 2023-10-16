f9ebcff1e80caffa4659b9b19e87a365d150415a
tobysha <892715964@qq.com>
2023-10-16 18:19:26 +0100

    V 0.2
    Add two of the platforms(safety, Rotating)

---
+++ Assets/Scripts/RotatingPlatformLogic.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class RotatingPlatformLogic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 angles = this.transform.localEulerAngles;
+        angles.y += 30 * Time.deltaTime;
+        this.transform.localEulerAngles = angles;
+    }
+}
