0534511bd7c9110f8735e7b2a307fc5e5ce24f6e
Logun John Tetley <48089813+loguntetley@users.noreply.github.com>
2023-11-01 09:50:17 +0000

    UI Interface
    currently selected platform

---
+++ Assets/Scripts/Data/PlatformData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlatformData : MonoBehaviour
+{
+    public string platformName = "No Platform";
+    public Texture2D platfromImage = null;
+}
---
+++ Assets/Scripts/UI/PlatformDisplayer.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlatformDisplayer : MonoBehaviour
+{
+    [SerializeField] private Texture2D displayTexture = null;
+    [SerializeField] private string displayName = "";
+    private PlayerData playerData;
+    [SerializeField] private RawImage image;
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private TextMeshProUGUI currentlySelectedText;
+
+    private void Start()
+    {
+        playerData = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerData>();
+    }
+
+    void Update()
+    {
+        SetUIActive();
+        GetPlatformData();
+        displayPlatformData();
+    }
+
+    private void GetPlatformData()
+    {
+        if (playerData.selectedPlatform != null)
+        {
+            displayTexture = playerData.selectedPlatform.GetComponent<PlatformData>().platfromImage;
+            displayName = playerData.selectedPlatform.GetComponent<PlatformData>().platformName;
+        }
+    }
+
+    private void displayPlatformData()
+    {
+        if (playerData.selectedPlatform != null) 
+        {
+            image.texture = displayTexture;
+            text.text = displayName;
+        }
+    }
+
+    private void SetUIActive()
+    {
+        if (playerData.selectedPlatform != null)
+        {
+            image.enabled = true;
+            text.enabled = true;
+            currentlySelectedText.enabled = true;
+        }
+        if (playerData.selectedPlatform == null)
+        {
+            image.enabled = false;
+            text.enabled = false;
+            currentlySelectedText.enabled = false;
+        }
+    }
+}
