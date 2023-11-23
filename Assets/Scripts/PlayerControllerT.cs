93378eaa94c32db881d72f8b150dfb893786bdfc
yushuai622 <qwe852asa@hotmail.com>
2023-11-23 22:25:45 +0000

    1
    1

--- Assets/Scripts/PlayerControllerT.cs
+++ Assets/Scripts/PlayerControllerT.cs
@@ -10,11 +10,13 @@ using Unity.VisualScripting;
 
 public class PlayerControllerT : MonoBehaviour
 {
-    [SerializeField] private float speed,global_speed, jumpAmount, velocityCap = 100;
+    [SerializeField] private float speed, global_speed, jumpAmount, velocityCap = 100;
     private Vector2 moveValue;
     private Rigidbody rigidBody;
     private PlayerData playerData;
     private bool isGrounded = true;
+    public float fullspeed = 4;
+    private bool high = false;
 
 
     private void Start()
@@ -24,7 +26,7 @@ public class PlayerControllerT : MonoBehaviour
     }
     void Update()
     {
-
+        jumpimprove();
     }
 
     void OnPlatformselection()
@@ -38,10 +40,10 @@ public class PlayerControllerT : MonoBehaviour
             playerData.selectedPlatform = hit.collider.gameObject;
         }
     }
-    
+
     private void OnJump()
     {
-        if ( isGrounded)
+        if (isGrounded)
         {
             rigidBody.AddForce(Vector2.up * jumpAmount, ForceMode.Impulse);
             isGrounded = false;
@@ -55,20 +57,17 @@ public class PlayerControllerT : MonoBehaviour
 
     private void FixedUpdate()
     {
-        //Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
-        //movement = transform.TransformDirection(movement);
-        //Player_movement_improvement();
-        //rigidBody.AddForce(movement * speed * Time.fixedDeltaTime);
-        //if (rigidBody.velocity.sqrMagnitude > velocityCap)
-        //    rigidBody.velocity *= 0.99f;
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        transform.position += new Vector3(moveHorizontal * speed * Time.deltaTime, 0, moveVertical * speed * Time.deltaTime);
+        Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
+        movement = transform.TransformDirection(movement);
+        Player_movement_improvement();
+        rigidBody.AddForce(movement * speed * Time.fixedDeltaTime);
+        if (rigidBody.velocity.sqrMagnitude > velocityCap)
+            rigidBody.velocity *= 0.99f;
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.tag == "Ground" || other.gameObject.tag == "DeathPlatform" || other.gameObject.tag == "PermanentDeathPlatform"|| other.gameObject.tag =="Selectable")
+        if (other.gameObject.tag == "Ground" || other.gameObject.tag == "DeathPlatform" || other.gameObject.tag == "PermanentDeathPlatform" || other.gameObject.tag == "Selectable")
         {
             isGrounded = true;
         }
@@ -76,13 +75,29 @@ public class PlayerControllerT : MonoBehaviour
     private void Player_movement_improvement()
     {
         Vector3 current_Velocity = rigidBody.velocity;
-        if(current_Velocity.x - moveValue.x > current_Velocity.x|| current_Velocity.y - moveValue.y > current_Velocity.y)
+        if (current_Velocity.x - moveValue.x > current_Velocity.x || current_Velocity.y - moveValue.y > current_Velocity.y)
         {
             speed = global_speed;
         }
         else
         {
             speed = global_speed / 1.5f;
+        }
+    }
+    private void jumpimprove()
+    {
+        if (rigidBody.velocity.y > 3f)
+        {
+            high = true;
+        }
+        if (rigidBody.velocity.y < 2.5f && high)
+        {
+            Physics.gravity = new Vector3(0, -9.8f * fullspeed, 0);
+            high = false;
+        }
+        if (isGrounded)
+        {
+            Physics.gravity = new Vector3(0, -12f, 0);
         }
     }
 }
